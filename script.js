d498b44930c64070c0577aed26c3d176e0457445
rill-glitch <pebimnn@gmail.com>
2026-01-13 23:49:47 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-function login() {
-  const user = document.getElementById("username").value;
-  const pass = document.getElementById("password").value;
-
-  if (user === "admin" && pass === "12345") {
-    localStorage.setItem("isLogin", "true");
-    window.location.replace("index.html");
-  } else {
-    const popup = document.getElementById("popupError");
-    popup.style.display = "flex";
-
-    // auto close setelah 2 detik
-    setTimeout(() => {
-      popup.style.display = "none";
-    }, 2000);
-  }
+/* ===============================
+   HAMBURGER MENU (MOBILE)
+=============================== */
+function toggleMenu() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("show");
 }
 
-function closePopup() {
-  document.getElementById("popupError").style.display = "none";
-}
+/* ===============================
+   AUTO TUTUP MENU SAAT LINK DIKLIK
+=============================== */
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll(".menu a");
+
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      document.querySelector(".menu").classList.remove("show");
+    });
+  });
+});
